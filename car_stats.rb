f338526e1871c14b7e41e22abbee5fef3dc5f5f2
Penkar <jspenkar@gmail.com>
2014-08-28 11:48:09 -0500

    Moddiefied car stat

--- car_stats.rb
+++ car_stats.rb
@@ -1,33 +1,19 @@
 class CarStats
-	# attr_accessor :color, :cars
-	# @@cars = {}
-	
-	# def initialize(color)
-	# 	@color = color	
-	# 	if @@cars.keys.include?(color)
-	# 		@@cars[color] += 1
-	# 	else
-	# 		@@cars[color]=1
-	# 	end
-	# end
+	def self.carhash(x)
+		@@carscolors = Hash.new(0)
+		x.each{|car| @@carscolors[car.color]+=1}
+		@@carscolors
+	end
 
 	def self.calc_top_color(x)
-		counts = Hash.new(0)
-		x.each do |car| 
-			counts[car.color]+=1
-		end
-		tops = counts.max_by{|x,y| y }
+		carhash(x)
+		tops = @@carscolors.max_by{|x,y| y }
 		tops.first
 	end
 
 	def self.calc_bottom_color(x)
-		counts = Hash.new(0)
-		x.each do |car| 
-			counts[car.color]+=1
-		end
-		tops = counts.min_by{|x,y| y }
-		tops.first
+		carhash(x)
+		bottom = @@carscolors.min_by{|x,y| y }
+		bottom.first
 	end
-
-
 end
